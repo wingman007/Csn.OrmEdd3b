44a26497d28b0e67ee9722460555fdfe95500c8e
Stoyan Cheresharov <cheresharov@hotmail.com>
2016-11-30 16:40:27 +0200

    Adding SPA project Empty Web App

---
+++ Csn.OrmEdd3b.Spa/Scripts/core.js
@@ -0,0 +1,65 @@
+﻿//all angular code for our app 
+// public/core.js
+var fmiFe = angular.module('CsnOrmEdd', []);
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+
+    // when landing on the page, get all todos and show them
+    $http.get('http://localhost:1977/api/persons/')
+		.success(function (data) {
+		    $scope.persons = data;
+		    console.log(data);
+		})
+		.error(function (data) {
+		    console.log('Error: ' + data);
+		});
+
+    // when submitting the add form, send the text to the node API
+    $scope.createPerson = function () {
+        $http.post('http://localhost:1977/api/persons/', $scope.formData)
+			.success(function (data) {
+			    $scope.persons.push($scope.formData);
+			    $scope.formData = {}; // clear the form so our user is ready to enter another
+			    // $scope.persons = data
+			    console.log(data);
+			})
+			.error(function (data) {
+			    console.log('Error: ' + data);
+			});
+    };
+
+    // Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'http://localhost:61451' is therefore not allowed access. The response had HTTP status code 400.
+    // delete a todo after checking it
+    $scope.deletePerson = function (id) {
+        $http.delete('http://localhost:1977/api/persons/' + id)
+			.success(function (data) {
+			    $scope.persons = data;
+			    console.log(data);
+			})
+			.error(function (data) {
+			    console.log('Error: ' + data);
+			});
+    };
+}
+
+/*
+[
+  {
+    "Id": 2,
+    "Name": "Stoyan",
+    "FamilyName": "Cheresharov",
+    "BirthDate": "1976-09-09T00:00:00",
+    "Address": "7 Avliga str",
+    "Phones": null
+  },
+  {
+    "Id": 4,
+    "Name": "sample string 2",
+    "FamilyName": "sample string 3",
+    "BirthDate": "2016-11-29T15:23:37.877",
+    "Address": "sample string 5",
+    "Phones": null
+  }
+]
+*/
--- Csn.OrmEdd3b.WebApi/App_Start/WebApiConfig.cs
+++ Csn.OrmEdd3b.WebApi/App_Start/WebApiConfig.cs
@@ -33,8 +33,10 @@ namespace Csn.OrmEdd3b.WebApi
             // 2. CORS Cross Origin Resource Sharing
             // Nuget package: install-package Microsoft.AspNet.WebApi.Cors
 
-            EnableCorsAttribute cors = new EnableCorsAttribute("http://localhost:1843,http://coolcsn.com", "*", "GET,POST"); // "*" - for all websites | "Accept,Content-type" or "*" | GET,POST
+            EnableCorsAttribute cors = new EnableCorsAttribute("http://localhost:1843,http://coolcsn.com,http://localhost:61451", "Accept,Content-type", "GET,POST"); // "*" - for all websites | "Accept,Content-type" or "*" | GET,POST or "*" for all
             config.EnableCors(cors);
+
+            // Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'http://localhost:61451' is therefore not allowed access. The response had HTTP status code 400.
         }
     }
 }
